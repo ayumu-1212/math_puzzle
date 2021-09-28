99d5e423ee1194db465c1372cbcf6527de859a5b
ayumu-1212 <ayumuabe1434@gmail.com>
2021-09-28 19:23:03 +0900

    :construction:

---
+++ q_42.rb
@@ -0,0 +1,52 @@
+" Press ? for help
+
+.. (up a dir)
+</programing/math_puzzle/
+▸ .git/
+▸ sample/
+  q_06.rb*
+  q_07.rb*
+  q_08.rb*
+  q_08_a.rb*
+  q_09.rb*
+  q_09_a.rb*
+  q_10.rb*
+  q_11.rb*
+  q_12.rb*
+  q_13.rb*
+  q_13_a.rb*
+  q_14.rb*
+  q_14_2.rb*
+  q_15.rb*
+  q_16.rb*
+  q_17.rb*
+  q_18.rb*
+  q_18_2.rb*
+  q_19.rb*
+  q_20.rb*
+  q_21.rb*
+  q_22.rb*
+  q_23.rb*
+  q_24.rb*
+  q_25.rb*
+  q_26.rb*
+  q_27.rb*
+  q_28.rb*
+  q_29.rb*
+  q_29_2.rb*
+  q_30.rb*
+  q_31.rb*
+  q_32.rb*
+  q_33.rb*
+  q_33_library.rb*
+  q_34.rb*
+  q_35.rb*
+  q_36.rb*
+  q_36_2.rb*
+  q_37.rb*
+  q_38.rb*
+  q_39.rb*
+  q_40.rb*
+  q_40_2.rb*
+  q_40_3.rb*
+  q_41.rb*
