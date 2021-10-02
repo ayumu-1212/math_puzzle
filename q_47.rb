01c52acd0c0b9fe2eded6515976a93d3fc94971c
ayumu-1212 <ayumuabe1434@gmail.com>
2021-10-02 17:25:29 +0900

    :construction:

---
+++ q_47.rb
@@ -0,0 +1,22 @@
+require "pry"
+
+N = 2
+
+def search(bit, width, height, is_right)
+  if is_right
+    count = []
+    height.times do |h|
+      cnt = 0
+      width.times do |w|
+        cnt += bit % 2
+        bit = bit / 2
+      end
+      count.push(cnt)
+    end
+  else
+  end
+end
+
+(2**(N**2)-1).times do |bit|
+  search(bit, N, N, true)
+end
