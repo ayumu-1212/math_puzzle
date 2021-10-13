e171f414563fe7bfca8c4e001a2b531fad52a117
ayumu-1212 <ayumuabe1434@gmail.com>
2021-10-13 12:15:23 +0900

    :sparkles: 55 rank: a

---
+++ q_55.rb
@@ -0,0 +1,45 @@
+require "benchmark"
+MIN, MAX = 1, 10
+
+def soroban(remain_nums, nums_sum, count)
+  return if @min < count
+  if remain_nums.empty?
+    @min = count
+    return
+  end
+  prev_memo = @memo[nums_sum]
+  remain_nums.each do |num|
+    next_sum = nums_sum + num
+    if !@memo.has_key?(next_sum)
+      tamas = []
+      instant_sum = [0, next_sum]
+      @digit.times do |n|
+        instant_sum = instant_sum[1].divmod(5 * 10 ** (@digit - n - 1))
+        tamas.push(instant_sum[0])
+        instant_sum = instant_sum[1].divmod(1 * 10 ** (@digit - n - 1))
+        tamas.push(instant_sum[0])
+      end
+      @memo[next_sum] = tamas
+    end
+    instant_count = 0
+    this_memo = @memo[next_sum]
+    (@digit * 2).times do |i|
+      instant_count += (prev_memo[i] - this_memo[i]).abs
+    end
+    soroban(remain_nums - [num], next_sum, count + instant_count)
+  end
+end
+
+@digit = [*MIN..MAX].sum.to_s.size
+
+@memo = {}
+@memo[0] = Array.new(@digit * 2, 0)
+
+@min = @digit * 9 * (MAX - MIN + 1)
+
+result = Benchmark.realtime do
+soroban([*MIN..MAX], 0, 0)
+end
+
+puts @min
+puts "time: #{result}s"
